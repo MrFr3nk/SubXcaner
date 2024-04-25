abd9e3f2bba382ca31ace94a0b84a2686591d167
ᴹᴿ᭄ Fʀᴀɴᴋ ᴏғᴄ 々 <darrelmucheri@gmail.com>
2024-04-25 18:19:09 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,7 +57,7 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'SIGMA-MD;;;' + b64data });
 
                let SIGMA_MD_TEXT = `
-*_Pair Code By Maher Zubair_*
+*_Pair Code By Mr Frank_*
 *_Made With 🤍_*
 
 _Don't Forget To Give Star To My Repo_`
